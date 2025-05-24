cfdf452c06f5711c1d9ebef0c50c8c8b74f62131
MarcWije <marcwijesuriya@gmail.com>
2025-05-24 19:32:55 +0530

    Auto commit on 2025-05-24 19:32:55

--- ssh.bat
+++
@@ -1,5 +0,0 @@
-ssh-keygen -t ed25519 -C "marcwijesuriya@gmail.com"
-# Press enter to save in default location (~/.ssh/id_ed25519)
-# Add the key to the ssh agent
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
